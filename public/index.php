24b26a062912ecffaf1c4cfffc6aa46c3125f2a0
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-07-27 00:48:24 +0300

    Finish

--- public/index.php
+++ public/index.php
@@ -3,8 +3,6 @@ namespace App;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use function Stringy\create as s;
-
 $repo = new Repository();
 
 $configuration = [
@@ -27,19 +25,37 @@ $app->get('/', function ($request, $response) {
 });
 
 $app->post('/cart-items', function ($request, $response) {
+
     // Информация о добавляемом товаре
     $item = $request->getParsedBodyParam('item');
 
+    
+
     // Данные корзины
-    $cart = json_decode($request->getCookieParam('cart', json_encode([])));
+    $cart = json_decode($request->getCookieParam('cart', json_encode([])), true);
 
     // Добавление нового товара
-    $cart[] = $item;
+    if (empty($cart[$item['id']])){
+        $cart[$item['id']] = ['name' => $item['name'], 'count' => 1];
+        $encodedCart = json_encode($cart);
+        return $response->withHeader('Set-Cookie', "cart={$encodedCart}")
+        ->withRedirect('/');
+    }
+
+    $cart[$item['id']]['count'] += 1;
 
     // Кодирование корзины
     $encodedCart = json_encode($cart);
 
     // Установка новой корзины в куку
+    return $response->withHeader('Set-Cookie', "cart={$encodedCart}")
+        ->withRedirect('/');
+});
+
+$app->delete('/cart-items', function ($request, $response, array $args) use ($repo) {
+    $cart = json_decode($request->getCookieParam('cart', json_encode([])));
+    $cart = [];
+    $encodedCart = json_encode($cart);
     return $response->withHeader('Set-Cookie', "cart={$encodedCart}")
         ->withRedirect('/');
 });
