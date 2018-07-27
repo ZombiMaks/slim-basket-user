0bfb33ff8296d53343db031f096e11a0a2ba324e
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-07-27 22:55:17 +0300

    Session

--- public/index.php
+++ public/index.php
@@ -17,48 +17,33 @@ $container = $app->getContainer();
 $container['renderer'] = new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 
 $app->get('/', function ($request, $response) {
-    $cart = json_decode($request->getCookieParam('cart', json_encode([])), true);
-    $params = [
-        'cart' => $cart
-    ];
-    return $this->renderer->render($response, 'index.phtml', $params);
-});
-
-$app->post('/cart-items', function ($request, $response) {
-
-    // Информация о добавляемом товаре
-    $item = $request->getParsedBodyParam('item');
-
     
-
-    // Данные корзины
-    $cart = json_decode($request->getCookieParam('cart', json_encode([])), true);
-
-    // Добавление нового товара
-    if (empty($cart[$item['id']])){
-        $cart[$item['id']] = ['name' => $item['name'], 'count' => 1];
-        $encodedCart = json_encode($cart);
-        return $response->withHeader('Set-Cookie', "cart={$encodedCart}")
-        ->withRedirect('/');
-    }
-
-    $cart[$item['id']]['count'] += 1;
-
-    // Кодирование корзины
-    $encodedCart = json_encode($cart);
-
-    // Установка новой корзины в куку
-    return $response->withHeader('Set-Cookie', "cart={$encodedCart}")
-        ->withRedirect('/');
-});
-
-$app->delete('/cart-items', function ($request, $response, array $args) use ($repo) {
-    $cart = json_decode($request->getCookieParam('cart', json_encode([])));
-    $cart = [];
-    $encodedCart = json_encode($cart);
-    return $response->withHeader('Set-Cookie', "cart={$encodedCart}")
-        ->withRedirect('/');
-});
+    $params = [
+        'user' => $_SESSION['user']
+        
+    ];
+    
+     return $this->renderer->render($response, 'index.phtml', $params);
+ });
+ 
+ $app->post('/session', function ($request, $response) {
+ 
+     // Информация о добавляемом пользователе
+     $user = $request->getParsedBodyParam('user');
+ 
+     // Добавление нового пользователя
+         $_SESSION['user'] = $user;
+    
+    return $response->withRedirect('/');
+ });
+ 
+ $app->delete('/session', function ($request, $response) {
+     
+     session_unset();
+     session_destroy();
+ 
+     return $response->withRedirect('/');
+ });
 
 $app->run();
 
